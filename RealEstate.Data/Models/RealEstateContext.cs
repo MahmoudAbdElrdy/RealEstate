7640ddfcb5e5d0898cb6ad243d19c07a21ccc22b
MahmoudAbdElrdy <47526228+MahmoudAbdElrdy@users.noreply.github.com>
2021-11-29 23:07:29 +0200

    local

--- RealEstate.Data/Models/RealEstateContext.cs
+++ RealEstate.Data/Models/RealEstateContext.cs
@@ -63,7 +63,7 @@ namespace RealEstate.Data.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-LRTAREA;Database=RealEstate;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=RealEstate;Trusted_Connection=True;");
             }
         }
 
